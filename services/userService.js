75a62982947bb83a7ce6a450a1684260c65396e5
Pedro Ferraz <psoaresferraz@gmail.com>
2020-10-19 18:36:39 -0300

    corrige formatação

--- services/userService.js
+++ services/userService.js
@@ -1,55 +1,55 @@
-const { 
-    updateUser, 
-    findUserById, 
-    findUserByUsername,
-    createUser, 
-    findUserByUsernameAndPassword
- } = require('../repositories/userRepository')
+const {
+  updateUser,
+  findUserById,
+  findUserByUsername,
+  createUser,
+  findUserByUsernameAndPassword
+} = require('../repositories/userRepository')
 
 const { getChatIdForNewFriends } = require('./chatService')
 
 const userSignup = async (username, password) => {
-	if (!username || !password) {
-        throw new Error('Username and password are required')
-    }
-    if (await findUserByUsername(username)) {
-        throw new Error('Username already in use')
-    }    
-    await createUser(username, password)
+  if (!username || !password) {
+    throw new Error('Username and password are required')
+  }
+  if (await findUserByUsername(username)) {
+    throw new Error('Username already in use')
+  }
+  await createUser(username, password)
 }
 
 const userSignin = async (username, password) => {
-    if (!username || !password) {
-        throw new Error('Username and password are required')
-    }
-    if (!(await findUserByUsernameAndPassword(username, password))) {
-        throw new Error('incorrect username or password')
-    }
+  if (!username || !password) {
+    throw new Error('Username and password are required')
+  }
+  if (!(await findUserByUsernameAndPassword(username, password))) {
+    throw new Error('incorrect username or password')
+  }
 }
 
 const addUserFriendByUsername = async (userId, friendUsername) => {
-    if (!friendUsername) {
-        throw new Error('Friend username is required')
-    }
+  if (!friendUsername) {
+    throw new Error('Friend username is required')
+  }
 
-    const user = await findUserById(userId)
-    const friend = await findUserByUsername(friendUsername)
+  const user = await findUserById(userId)
+  const friend = await findUserByUsername(friendUsername)
 
-    if (!friend) {
-        throw new Error('Friend username is not found')
-    }
+  if (!friend) {
+    throw new Error('Friend username is not found')
+  }
 
-    user.friends.push(friend.id)
+  user.friends.push(friend.id)
 
-    const chatId = await getChatIdForNewFriends([user.id, friend.id])
+  const chatId = await getChatIdForNewFriends([user.id, friend.id])
 
-    user.chats.push(chatId)
+  user.chats.push(chatId)
 
-    updateUser(user)
+  updateUser(user)
 }
 
 module.exports = {
-    userSignup,
-    userSignin,
-    addUserFriendByUsername
+  userSignup,
+  userSignin,
+  addUserFriendByUsername
 }
