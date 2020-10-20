3fc8fd19fbc660313097bdf0a1e2994b0e58e832
Pedro Soares Ferraz <psoaresferraz@gmail.com>
2020-10-19 21:13:36 -0300

    refatora nome de métodos em chatService

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const ChatClient = require('../facades/ChatClient')
 const { verifyJWT, verifyJWTQueryString } = require('../middlewares/authMiddleware')
-const { connectClient, disconnectClient, sendMessage } = require('../services/chatService')
+const { connectChatClient, disconnectChatClient, sendChatMessage } = require('../services/chatService')
 
 const router = express.Router();
 
 router.get('/chat/subscribe', verifyJWTQueryString, async (req, res, next) => {
-    try {
-        var client = new ChatClient(req.user.id, res)
+  try {
+    var client = new ChatClient(req.user.id, res)
 
-        client.onClose(() => {
-            disconnectClient(client.id)
-        })
+    client.onClose(() => {
+      disconnectChatClient(client.id)
+    })
 
-        await connectClient(client)
-    } catch (e) {
-        next(e)
-    }
+    await connectChatClient(client)
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.post('/chat/sendMessage', verifyJWT, async (req, res, next) => {
-    try {
-        await sendMessage(req.body.chatId, req.body.text)
+  try {
+    await sendChatMessage(req.body.chatId, req.body.text)
 
-        res.json({
-            result: 'success'
-        })
-    } catch (e) {
-        next(e)
-    }
+    res.json({
+      result: 'success'
+    })
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = router
--- services/chatService.js
+++ services/chatService.js
@@ -2,60 +2,60 @@ const { createChat, findChatById, findChatBySubscribers } = require('../reposito
 const { createOfflineMessage, findOfflineMessagesByUserId, deleteOfflineMessagesByUserId } = require('../repositories/messageRepository')
 
 const getChatIdForNewFriends = async (friends) => {
-    const chat = await findChatBySubscribers(friends)
+  const chat = await findChatBySubscribers(friends)
 
-    let chatId
+  let chatId
 
-    if (!chat) {
-        chatId = await createChat(friends)
-    }
+  if (!chat) {
+    chatId = await createChat(friends)
+  }
 
-    return chatId || chat.id
+  return chatId || chat.id
 }
 
 const clients = {}
 
-const connectClient = async (client) => {
-    clients[client.id] = client
+const connectChatClient = async (client) => {
+  clients[client.id] = client
 
-    console.log('Conecting client ' + client.id)
+  console.log('Conecting client ' + client.id)
 
-    const offlineMessages = await findOfflineMessagesByUserId(client.id)
+  const offlineMessages = await findOfflineMessagesByUserId(client.id)
 
-    if (offlineMessages) {
-        console.log('Sending queued messages to ' + client.id)
-        client.sendMessages(offlineMessages)
-        await deleteOfflineMessagesByUserId(client.id)
+  if (offlineMessages) {
+    console.log('Sending queued messages to ' + client.id)
+    client.sendMessages(offlineMessages)
+    await deleteOfflineMessagesByUserId(client.id)
+  }
+}
+
+const disconnectChatClient = async (clientId) => {
+  delete clients[clientId]
+}
+
+const sendChatMessage = async (chatId, messageText) => {
+  const chat = await findChatById(chatId)
+
+  chat.subscribers.forEach(async subscriberId => {
+    let client = clients[subscriberId]
+
+    if (client) {
+      console.log('Sending message to ' + subscriberId)
+      client.sendMessage({
+        chatId,
+        text: messageText
+      })
     }
-}
-
-const disconnectClient = async (clientId) => {
-    delete clients[clientId]
-}
-
-const sendMessage = async (chatId, messageText) => {
-    const chat = await findChatById(chatId)
-
-    chat.subscribers.forEach(async subscriberId => {
-        let client = clients[subscriberId]
-
-        if (client) {
-            console.log('Sending message to ' + subscriberId)
-            client.sendMessage({ 
-                chatId,
-                text: messageText
-            })
-        }
-        else {
-            console.log('Enqueuing messages to ' + subscriberId)
-            await createOfflineMessage(subscriberId, chatId, messageText)
-        }
-    })
+    else {
+      console.log('Enqueuing messages to ' + subscriberId)
+      await createOfflineMessage(subscriberId, chatId, messageText)
+    }
+  })
 }
 
 module.exports = {
-    getChatIdForNewFriends,
-    connectClient,
-    disconnectClient,
-    sendMessage
+  getChatIdForNewFriends,
+  connectChatClient,
+  disconnectChatClient,
+  sendChatMessage
 }
